8bec0c412eac9563dc92281a8ae9c11ea1e52f32
Ivan Vasylyshyn <ivan.vasylyshyn@digit.az>
2023-09-22 12:35:34 +0300

    Add glfw, opengl and base window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
-#include <glfw3.h>
+#include "src/graphics/window.h"
 
 int main()
 {
+    using namespace moon;
+    using namespace graphics;
+
+    Window window("Moon", 960, 540);
+    glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+
+     while(!window.closed())
+    {
+        window.clear();
+        glBegin(GL_QUADS);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(-0.5f, 0.5f);
+        glVertex2f(0.5f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        window.update();
+    }
 
     return 0;
 }
