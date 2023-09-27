7413dde9955be4d3bfd591fe26ea2fd85a469ed7
Ivan Vasylyshyn <ivan.vasylyshyn@digit.az>
2023-09-27 10:26:57 +0300

    Add vec2, vec3, vec4

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "src/graphics/window.h"
+#include "src/maths/maths.h"
 
 int main()
 {
     using namespace moon;
     using namespace graphics;
+    using namespace maths;
 
     Window window("Moon", 960, 540);
     glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
@@ -12,13 +14,15 @@ int main()
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
 
+    vec4 a(1.0f, 2.0f, 5.0f, 1.0f);
+    vec4 b(2.0f, 4.0f, 6.0f, 1.0f);
+
      while(!window.closed())
     {
         window.clear();
+        std::cout << (a += b) << std::endl;
 
         double x, y;
-        window.getMousePosition(x, y);
-        std::cout << x << ", " << y << std::endl;
 
         glBegin(GL_QUADS);
         glVertex2f(-0.5f, -0.5f);
@@ -27,7 +31,6 @@ int main()
         glVertex2f(0.5f, -0.5f);
         glEnd();
 
-//        glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
         window.update();
     }
 
---
+++ src/maths/maths.h
@@ -0,0 +1,9 @@
+//
+// Created by Work on 27.09.2023.
+//
+
+#pragma once
+
+#include "vec2.h"
+#include "vec3.h"
+#include "vec4.h"
